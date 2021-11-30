8a747b2b77bd2875647ceb149481f88feca06841
Vania Tavares <vntavares56@gmail.com>
2021-11-30 00:53:56 +0000

    users post and put updated and tested, movies post needs testing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const connection = require("./db-config");
 // Import expres
 const express = require("express");
+const Joi = require("joi");
+
 const movieRouter = require("./movie");
 
 // We store all express methods in a variable called app
@@ -11,196 +13,204 @@ const port = process.env.PORT || 3000;
 
 connection.connect((err) => {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error("error connecting: " + err.stack);
   } else {
-    console.log('connected as id ' + connection.threadId);
+    console.log("connected as id " + connection.threadId);
   }
 });
 
 app.use(express.json());
 
-app.get('/api/movies', (req, res) => {
-  let sql = 'SELECT * FROM movies';
-  const sqlValues = [];
-  if (req.query.color) {
-    sql += ' WHERE color = ?';
-    sqlValues.push(req.query.color);
-  }
-  if (req.query.max_duration) {
-    if (req.query.color) sql += ' AND duration <= ? ;';
-    else sql += ' WHERE duration <= ?';
-
-    sqlValues.push(req.query.max_duration);
-  }
-
-  connection.query(sql, sqlValues, (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send('Error retrieving movies from database');
-    } else {
-      res.json(results);
-    }
-  });
-});
-
-app.get('/api/movies/:id', (req, res) => {
-  const movieId = req.params.id;
-  connection.query(
-    'SELECT * FROM movies WHERE id = ?',
-    [movieId],
-    (err, results) => {
-      if (err) {
-        res.status(500).send('Error retrieving movie from database');
-      } else {
-        if (results.length) res.json(results[0]);
-        else res.status(404).send('Movie not found');
-      }
-    }
-  );
-});
-
-app.get('/api/users', (req, res) => {
-  let sql = 'SELECT * FROM users';
+app.get("/api/users", (req, res) => {
+  let sql = "SELECT * FROM users";
   const sqlValues = [];
   if (req.query.language) {
-    sql += ' WHERE language = ?';
+    sql += " WHERE language = ?";
     sqlValues.push(req.query.language);
   }
   connection.query(sql, sqlValues, (err, results) => {
     if (err) {
-      res.status(500).send('Error retrieving users from database');
+      res.status(500).send("Error retrieving users from database");
     } else {
       res.json(results);
     }
   });
 });
 
-app.get('/api/users/:id', (req, res) => {
+app.get("/api/users/:id", (req, res) => {
   const userId = req.params.id;
   connection.query(
-    'SELECT * FROM users WHERE id = ?',
+    "SELECT * FROM users WHERE id = ?",
     [userId],
     (err, results) => {
       if (err) {
-        res.status(500).send('Error retrieving user from database');
+        res.status(500).send("Error retrieving user from database");
       } else {
         if (results.length) res.json(results[0]);
-        else res.status(404).send('User not found');
+        else res.status(404).send("User not found");
       }
     }
   );
 });
 
-app.post('/api/movies', (req, res) => {
-  const { title, director, year, color, duration } = req.body;
-  connection.query(
-    'INSERT INTO movies (title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)',
-    [title, director, year, color, duration],
-    (err, result) => {
-      if (err) {
-        res.status(500).send('Error saving the movie');
-      } else {
-        const id = result.insertId;
-        const createdMovie = { id, title, director, year, color, duration };
-        res.status(201).json(createdMovie);
-      }
-    }
-  );
-});
-
-app.post('/api/users', (req, res) => {
+/*
+app.post("/api/users", (req, res) => {
   const { firstname, lastname, email } = req.body;
-  connection.query(
-    'INSERT INTO users (firstname, lastname, email) VALUES (?, ?, ?)',
-    [firstname, lastname, email],
-    (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error saving the user');
+
+  const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
+
+  const errors = [];
+  const db = connection.promise();
+
+  const hardValidation = (arg) => {
+  if(!arg) errors.push({ field: `${arg}`, message: "This field is required" });
+  else if(arg === "email" && !emailRegex.test(email))
+    errors.push({ field: "email", message: "Invalid email" });
+    else if((arg === "firstname" || arg === "lastname") && arg.length >= 255) {
+    errors.push({
+      field: `${arg}`,
+      message: "Should contain less than 255 characters",
+    });
+    }
+  }
+
+  hardValidation(firstname);
+  hardValidation(laststname);
+  hardValidation(email);
+
+  db.query("SELECT email FROM users WHERE email=?;", [email]).then(
+    ([result]) => {
+      if (result[0]) {
+        errors.push({
+          field: "email",
+          message: "There's an user with that email",
+        });
+      }
+      if (
+        errors.length &&
+        errors.find((elt) => elt.message == "There's an user with that email")
+      ) {
+        res.status(409).json({ message: "This email is already used" });
+      } else if (errors.length) {
+        res.status(422).json({ validationErrors: errors });
       } else {
-        const id = result.insertId;
-        const createdUser = { id, firstname, lastname, email };
-        res.status(201).json(createdUser);
+        db.query(
+          "INSERT INTO users (firstname, lastname, email) VALUES (?, ?, ?)",
+          [firstname, lastname, email]
+        )
+          .then(([{ insertId }]) => {
+            res.status(201).json({ id: insertId, firstname, lastname, email });
+          })
+          .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error saving the user");
+          });
       }
     }
   );
 });
+*/
 
-app.put('/api/users/:id', (req, res) => {
+app.post("/api/users", (req, res) => {
+  const db = connection.promise();
+  const { firstname, lastname, email, city, language } = req.body;
+  let validationErrors = null;
+
+  db.query("SELECT email FROM users WHERE email=?", [email])
+    .then(([result]) => {
+      if (result[0]) return Promise.reject("DUPLICATE_EMAIL");
+      validationErrors = Joi.object({
+        email: Joi.string().email().max(255).required(),
+        firstname: Joi.string().max(255).required(),
+        lastname: Joi.string().max(255).required(),
+        city: Joi.string().max(255),
+        language: Joi.string().max(255),
+      }).validate(
+        { firstname, lastname, email, city, language },
+        { abortEarly: false }
+      ).error;
+      if (validationErrors) return Promise.reject("INVALID_DATA");
+      return db.query(
+        "INSERT INTO users (firstname, lastname, email, city, language ) VALUES (?, ?, ?, ?, ?);",
+        [firstname, lastname, email, city, language]
+      );
+    })
+    .then(([{ insertId }]) => {
+      res
+        .status(201)
+        .json({ id: insertId, firstname, lastname, email, city, language });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err === "DUPLICATE_EMAIL")
+        res.status(409).json({ message: "This email is already in use." });
+      else if (err === "INVALID_DATA")
+        res.status(422).json({ validationErrors });
+      else res.status(500).send("Error saving the user");
+    });
+});
+
+app.put("/api/users/:id", (req, res) => {
   const userId = req.params.id;
+  const numericId = parseInt(userId);
+  let { firstname, lastname, email, city, language } = req.body;
   const db = connection.promise();
   let existingUser = null;
-  db.query('SELECT * FROM users WHERE id = ?', [userId])
+  let validationErrors = null;
+  db.query("SELECT id, email FROM users WHERE email=?;", [email])
+    .then(([result]) => {
+      if (result[0] && result[0].id !== numericId) {
+        return Promise.reject("DUPLICATE_EMAIL");
+      }
+      validationErrors = Joi.object({
+        email: Joi.string().email().max(255),
+        firstname: Joi.string().max(255),
+        lastname: Joi.string().max(255),
+        city: Joi.string().max(255),
+        language: Joi.string().max(255),
+      }).validate(
+        { firstname, lastname, email, city, language },
+        { abortEarly: false }
+      ).error;
+      if (validationErrors) return Promise.reject("INVALID_DATA");
+      return db.query("SELECT * FROM users WHERE id = ?", [userId]);
+    })
     .then(([results]) => {
       existingUser = results[0];
-      if (!existingUser) return Promise.reject('RECORD_NOT_FOUND');
-      return db.query('UPDATE users SET ? WHERE id = ?', [req.body, userId]);
+      if (!existingUser) return Promise.reject("RECORD_NOT_FOUND");
+      return db.query("UPDATE users SET ? WHERE id = ?", [req.body, userId]);
     })
     .then(() => {
       res.status(200).json({ ...existingUser, ...req.body });
     })
     .catch((err) => {
       console.error(err);
-      if (err === 'RECORD_NOT_FOUND')
+      if (err === "DUPLICATE_EMAIL")
+        res.status(409).send("Email already in use by other user");
+      else if (err === "INVALID_DATA")
+        res.status(422).json({ validationErrors });
+      else if (err === "RECORD_NOT_FOUND")
         res.status(404).send(`User with id ${userId} not found.`);
-      else res.status(500).send('Error updating a user');
+      else res.status(500).send("Error updating a user");
     });
 });
 
-app.put('/api/movies/:id', (req, res) => {
-  const movieId = req.params.id;
-  const db = connection.promise();
-  let existingMovie = null;
-  db.query('SELECT * FROM movies WHERE id = ?', [movieId])
-    .then(([results]) => {
-      existingMovie = results[0];
-      if (!existingMovie) return Promise.reject('RECORD_NOT_FOUND');
-      return db.query('UPDATE movies SET ? WHERE id = ?', [req.body, movieId]);
-    })
-    .then(() => {
-      res.status(200).json({ ...existingMovie, ...req.body });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err === 'RECORD_NOT_FOUND')
-        res.status(404).send(`Movie with id ${movieId} not found.`);
-      else res.status(500).send('Error updating a movie.');
-    });
-});
-
-app.delete('/api/users/:id', (req, res) => {
+app.delete("/api/users/:id", (req, res) => {
   connection.query(
-    'DELETE FROM users WHERE id = ?',
+    "DELETE FROM users WHERE id = ?",
     [req.params.id],
     (err, result) => {
       if (err) {
         console.log(err);
-        res.status(500).send('Error deleting an user');
+        res.status(500).send("Error deleting an user");
       } else {
-        if (result.affectedRows) res.status(200).send('🎉 User deleted!');
-        else res.status(404).send('User not found.');
-      }
-    }
-  );
-});
-
-app.delete('/api/movies/:id', (req, res) => {
-  const movieId = req.params.id;
-  connection.query(
-    'DELETE FROM movies WHERE id = ?',
-    [movieId],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Error deleting a movie');
-      } else {
-        if (result.affectedRows) res.status(200).send('🎉 Movie deleted!');
-        else res.status(404).send('Movie not found');
+        if (result.affectedRows) res.status(200).send("🎉 User deleted!");
+        else res.status(404).send("User not found.");
       }
     }
   );
 });
 
 app.listen(port, () => {
-	console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
--- movie.js
+++ movie.js
@@ -6,84 +6,121 @@ const movieRouter = express.Router();
 movieRouter.use(express.json());
 
 connection.connect((err) => {
-	if (err) {
-		console.error("error connecting: " + err.stack);
-	} else {
-		console.log(
-			"connected to database with threadId :  " + connection.threadId
-		);
-	}
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  } else {
+    console.log(
+      "connected to database with threadId :  " + connection.threadId
+    );
+  }
 });
 
 movieRouter.get("/", (req, res) => {
-	let sql = "SELECT * FROM movies";
-	const sqlFilter = [];
+  let sql = "SELECT * FROM movies";
+  const sqlFilter = [];
 
-	if (
-		(req.query.color && req.query.max_duration) ||
-		(req.query.max_duration && req.query.color)
-	) {
-		sql += " WHERE color = ? AND duration <= ?";
-		sqlFilter.push(req.query.color);
-		sqlFilter.push(req.query.max_duration);
-	} else if (req.query.color) {
-		sql += " WHERE color = ?";
-		sqlFilter.push(req.query.color);
-	} else if (req.query.max_duration) {
-		sql += " WHERE duration <= ?";
-		sqlFilter.push(req.query.max_duration);
-	}
+  if (
+    (req.query.color && req.query.max_duration) ||
+    (req.query.max_duration && req.query.color)
+  ) {
+    sql += " WHERE color = ? AND duration <= ?";
+    sqlFilter.push(req.query.color);
+    sqlFilter.push(req.query.max_duration);
+  } else if (req.query.color) {
+    sql += " WHERE color = ?";
+    sqlFilter.push(req.query.color);
+  } else if (req.query.max_duration) {
+    sql += " WHERE duration <= ?";
+    sqlFilter.push(req.query.max_duration);
+  }
 
-	console.log(sqlFilter);
+  console.log(sqlFilter);
 
-	connection.query(sql, sqlFilter, (err, result) => {
-		if (err) {
-			res.status(500).send("Error retrieving data from database");
-		} else {
-			if (result.length > 0) {
-				res.status(200).json(result);
-			} else {
-				res.status(200).json([]);
-			}
-		}
-	});
+  connection.query(sql, sqlFilter, (err, result) => {
+    if (err) {
+      res.status(500).send("Error retrieving data from database");
+    } else {
+      if (result.length > 0) {
+        res.status(200).json(result);
+      } else {
+        res.status(200).json([]);
+      }
+    }
+  });
 });
 
 movieRouter.get("/:id", (req, res) => {
-	connection.query(
-		"SELECT * FROM movies WHERE id = ?",
-		[req.params.id],
-		(err, result) => {
-			if (err) {
-				res.status(500).send("Error retrieving data from database");
-			} else {
-				if (result.length > 0) {
-					res.status(200).json(result[0]);
-				} else {
-					res.status(404).send("No movies found.");
-				}
-			}
-		}
-	);
+  connection.query(
+    "SELECT * FROM movies WHERE id = ?",
+    [req.params.id],
+    (err, result) => {
+      if (err) {
+        res.status(500).send("Error retrieving data from database");
+      } else {
+        if (result.length > 0) {
+          res.status(200).json(result[0]);
+        } else {
+          res.status(404).send("No movies found.");
+        }
+      }
+    }
+  );
 });
 
 movieRouter.post("/", (req, res) => {
-	const { title, director, year, color, duration } = req.body;
+  const { title, director, year, color, duration } = req.body;
+  const db = connection.promise();
 
-	connection.query(
-		"INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
-		[title, director, year, color, duration],
-		(err, result) => {
-			if (err) {
-				res.status(500).send("Error saving the movie");
-			} else {
-				console.log(result);
-				const id = result.insertId;
-				const createdUser = { id, title, director, year, color, duration };
-				res.status(201).json(createdUser);
-			}
-		}
-	);
+  const hardValidationString = (arg) => {
+    if (!arg)
+      errors.push({ field: `${arg}`, message: "This field is required" });
+    else if (arg.length >= 255 || typeof arg !== "string") {
+      console.log("+++++++++++++++ strings");
+      errors.push({
+        field: `${arg}`,
+        message: "Should contain less than 255 alpha-numeric characters",
+      });
+    } else return;
+  };
+
+  const hardValidationNumeric = (arg, value) => {
+    if (!arg)
+      errors.push({ field: `${arg}`, message: "This field is required" });
+    else if (arg <= value || typeof arg !== "number") {
+      console.log("+++++++++++++++ numberss");
+      errors.push({
+        field: `${arg}`,
+        message: `Should be a number bigger than ${value}`,
+      });
+    } else return;
+  };
+
+  hardValidationString(title);
+  hardValidationString(director);
+  hardValidationNumeric(year, 1887);
+  hardValidationNumeric(duration, 0);
+  if (!color || color > 1)
+    errors.push({
+      field: "color",
+      message: `Required with value of true or false (1 or 0)`,
+    });
+
+  if (error.length) res.status(422).json({ validationErrors: errors });
+  else {
+    db.query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+      .then(([{ insertId }]) => {
+        res
+          .status(201)
+          .json({ id: insertId, title, director, year, color, duration });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error saving the movie");
+      });
+  }
 });
 
 /*movieRouter.put("/:id", (req, res) => {
@@ -105,53 +142,53 @@ movieRouter.post("/", (req, res) => {
 */
 
 movieRouter.put("/:id", (req, res) => {
-	const movieId = req.params.id;
+  const movieId = req.params.id;
 
-	connection.query(
-		"SELECT * FROM movies WHERE id = ?",
-		[movieId],
-		(err, selectedResult) => {
-			if (err) {
-				res.status(500).send("Error!");
-			} else {
-				const dbresult = selectedResult[0];
-				if (dbresult) {
-					connection.query(
-						"UPDATE movies SET ? WHERE id = ?",
-						[req.body, movieId],
-						(err) => {
-							if (err) {
-								res.status(500).send("Error on retrieval");
-							} else {
-								const updatedMovie = { ...dbresult, ...req.body };
-								res.json(updatedMovie);
-							}
-						}
-					);
-				} else {
-					res
-						.status(404)
-						.send(`Movie id ${movieId} is not valid. Movie not found!`);
-				}
-			}
-		}
-	);
+  connection.query(
+    "SELECT * FROM movies WHERE id = ?",
+    [movieId],
+    (err, selectedResult) => {
+      if (err) {
+        res.status(500).send("Error!");
+      } else {
+        const dbresult = selectedResult[0];
+        if (dbresult) {
+          connection.query(
+            "UPDATE movies SET ? WHERE id = ?",
+            [req.body, movieId],
+            (err) => {
+              if (err) {
+                res.status(500).send("Error on retrieval");
+              } else {
+                const updatedMovie = { ...dbresult, ...req.body };
+                res.json(updatedMovie);
+              }
+            }
+          );
+        } else {
+          res
+            .status(404)
+            .send(`Movie id ${movieId} is not valid. Movie not found!`);
+        }
+      }
+    }
+  );
 });
 
 movieRouter.delete("/:id", (req, res) => {
-	const movieId = req.params.id;
-	connection.query(
-		"DELETE FROM movies WHERE id = ?",
-		[movieId],
-		(err, result) => {
-			if (err) {
-				console.log(err);
-				res.status(500).send("😱 Error deleting movie");
-			} else {
-				res.status(200).send("🎉 Movie deleted!");
-			}
-		}
-	);
+  const movieId = req.params.id;
+  connection.query(
+    "DELETE FROM movies WHERE id = ?",
+    [movieId],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("😱 Error deleting movie");
+      } else {
+        res.status(200).send("🎉 Movie deleted!");
+      }
+    }
+  );
 });
 
 module.exports = movieRouter;
