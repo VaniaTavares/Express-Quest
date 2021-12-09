48f095188b1b9504b7a80215640296a9a9460714
Vania Tavares <vntavares56@gmail.com>
2021-12-09 09:44:39 +0000

    async await error handling

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -8,7 +8,7 @@ const allMoviesController = async (req, res) => {
   try {
     if (req.cookies.user_token) {
       let user = await usersModel.getUserByTokenAsync(req.cookies.user_token);
-      if (!user) res.status(401).send("Unauthorized");
+      if (!user) throw new Error("INVALID_CREDENTIALS");
       req.body.id = user.id;
     }
     const { max_duration, color } = req.query;
@@ -20,37 +20,49 @@ const allMoviesController = async (req, res) => {
       req.body.id
     );
 
-    if (movies.length > 0) res.status(200).json(movies);
-    else if (movies.length === 0)
-      res.status(404).send("No movies found with those specifications");
+    if (movies.length === 0) throw new Error("NO_RECORD_FOUND");
+
+    res.status(200).json(movies);
   } catch (err) {
     console.log(err);
+    if ("INVALID_CREDENTIALS") res.status(401).send("Unauthorized");
+    else if (NO_RECORD_FOUND)
+      res.status(404).send("No movies found with those specifications");
     res.status(500).send("Internal issues");
   }
 };
 
 const movieByIdController = async (req, res) => {
-  const movieId = req.params.id;
-  let movie = await moviesModel.findMovieById(movieId);
-  if (movie.length > 0) res.status(200).json(movie[0]);
-  else if (movie.length === 0)
-    res.status(404).send("No movies found with that id");
-  else res.status(500).send("Error retrieving movies");
+  try {
+    const movieId = req.params.id;
+    console.log(movieId);
+    let movie = await moviesModel.findMovieById(movieId);
+    if (movie.length > 0) res.status(200).json(movie[0]);
+    else if (movie.length === 0) throw new Error("NO_RECORD_FOUND");
+  } catch (err) {
+    console.log(err);
+    if ("NO_RECORD_FOUND") res.status(404).send("No movies found with that id");
+    else res.status(500).send("Error retrieving movies");
+  }
 };
 
 const newMovieController = async (req, res) => {
   let errors;
-  errors = moviesModel.validateMovies(req.body);
-  if (errors && errors.length)
-    res.status(422).json({ validationErrors: errors });
-  else {
+  try {
+    errors = moviesModel.validateMovies(req.body);
+    if (errors && errors.length) throw new Error("INVALID_DATA");
+
     let user = await usersModel.getUserByTokenAsync(req.cookies.user_token);
-    if (!user) res.status(401).send("Unauthorized!");
+    if (!user) throw new Error("NO_USER_FOUND");
     req.body.user_id = user.id;
 
     let insertId = await moviesModel.insertNewMovie(req.body);
     delete req.body.user_id;
     if (insertId) res.status(201).json({ id: insertId, ...req.body });
+  } catch (err) {
+    console.log(err);
+    if ("NO_USER_FOUND") res.status(401).send("Unauthorized!");
+    else if ("INVALID_DATA") res.status(422).json({ validationErrors: errors });
     else res.status(500).send("Error saving the movie");
   }
 };
@@ -60,34 +72,37 @@ const updateMovieController = async (req, res) => {
   let errors;
   let existingMovie;
 
-  let movie = await moviesModel.findMovieById(movieId);
-  if (movie.length > 0) {
+  try {
+    let movie = await moviesModel.findMovieById(movieId);
+    if (movie.length === 0) throw new Error("NO_RECORD_FOUND");
     existingMovie = movie[0];
 
     errors = moviesModel.validateMovies(req.body, false);
 
-    if (errors.length) res.status(422).json({ validationErrors: errors });
-    else {
-      let movieToUpdate = await moviesModel.updateMovie(req.body, movieId);
-      if (movieToUpdate === 1)
-        res.status(200).json({ ...existingMovie, ...req.body });
-      else res.status(500).send("Error updating movie");
-    }
-  } else if (movie.length === 0)
-    res.status(404).send("No movies found with that id");
-  else res.status(500).send("Error retrieving movies");
+    if (errors.length) throw new Error("INVALID_DATA");
+
+    let movieToUpdate = await moviesModel.updateMovie(req.body, movieId);
+    if (movieToUpdate === 1)
+      res.status(200).json({ ...existingMovie, ...req.body });
+  } catch (err) {
+    console.log(err);
+    if ("NO_RECORD_FOUND") res.status(404).send("No movies found with that id");
+    else if ("INVALID_DATA") res.status(422).json({ validationErrors: errors });
+    else res.status(500).send("Error updating movies");
+  }
 };
 
 const deleteMovieByIdController = async (req, res) => {
   const movieId = req.params.id;
-
-  let deletedMovie = await moviesModel.deleteMovieById(movieId);
-
-  if (deletedMovie.affectedRows === 1)
+  try {
+    let deletedMovie = await moviesModel.deleteMovieById(movieId);
+    if (deletedMovie.affectedRows === 0) throw new Error("NO_RECORD_FOUND");
     res.status(200).send("🎉 Movie deleted!");
-  else if (deletedMovie.affectedRows === 0)
-    res.status(404).send(`Movie with id ${movieId} not found.`);
-  else res.status(500).send("😱 Error deleting movie");
+  } catch (err) {
+    if ("NO_RECORD_FOUND")
+      res.status(404).send(`Movie with id ${movieId} not found.`);
+    else res.status(500).send("😱 Error deleting movie");
+  }
 };
 module.exports = {
   allMoviesController,
